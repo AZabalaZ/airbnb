573729df20ed76fc237a27fc32c452f65525a487
augusto <augustozab27@gmail.com>
2024-02-28 14:34:22 -0300

    creacion rentas

---
+++ db/migrate/20240228153726_add_description_to_vehicles.rb
@@ -0,0 +1,5 @@
+class AddDescriptionToVehicles < ActiveRecord::Migration[7.1]
+  def change
+    add_column :vehicles, :description, :text
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -8,7 +8,7 @@
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
 
-Vehicle.create!(make: "Toyota", model: "Corolla", year: 2015, color: "Red", mileage: 50000, user: User.first)
-Vehicle.create!(make: "Honda", model: "Civic", year: 2016, color: "Blue", mileage: 40000, user: User.first)
-Vehicle.create!(make: "Ford", model: "Fusion", year: 2017, color: "Black", mileage: 30000, user: User.first)
-Vehicle.create!(make: "Chevrolet", model: "Malibu", year: 2018, color: "White", mileage: 20000, user: User.first)
+Vehicle.create!(make: "Toyota", model: "Corolla", year: 2015, color: "Red", mileage: 50000, user: User.first, description: "Great car, very reliable")
+Vehicle.create!(make: "Honda", model: "Civic", year: 2016, color: "Blue", mileage: 40000, user: User.first, description: "Great car, very reliable")
+Vehicle.create!(make: "Ford", model: "Fusion", year: 2017, color: "Black", mileage: 30000, user: User.first, description: "Great car, very reliable")
+Vehicle.create!(make: "Chevrolet", model: "Malibu", year: 2018, color: "White", mileage: 20000, user: User.first, description: "Great car, very reliable")

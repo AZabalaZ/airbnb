03790c7d95bc832cf9b352aaf42bbfe85963c54c
augusto <augustozab27@gmail.com>
2024-03-01 11:46:13 -0300

    cloudinary setup

--- Gemfile
+++ Gemfile
@@ -28,7 +28,7 @@ gem "jbuilder"
 
 gem "devise"
 
-
+gem "cloudinary"
 
 # Use Redis adapter to run Action Cable in production
 # gem "redis", ">= 4.0.1"
--- app/models/vehicle.rb
+++ app/models/vehicle.rb
@@ -2,5 +2,5 @@ class Vehicle < ApplicationRecord
   belongs_to :user
   has_many :bookings, dependent: :destroy
 
-  has_one_attached :photo
+  has_many_attached :photos
 end
